f1b25868e5281b80c9a1f8957883fb59d1635698
Tony Ho <tonyho1992@gmail.com>
2015-09-10 01:54:20 -0700

    fix bug with date delta

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,10 @@
 class WelcomeController < ApplicationController
   def show
     individuals = %w(Tony Sebastian Bo Marcus Dennis Tarik)
-    @person1 = individuals[(Date.today - Date.new(2015,1,1)).days % individuals.length]
+    @person1 = individuals[(Date.today - Date.new(2015,1,1)).to_i % individuals.length]
     individuals.shift(individuals.length / 3)
-    @person2 = individuals[(Date.today - Date.new(2015,1,1)).days% individuals.length]
+    @person2 = individuals[(Date.today - Date.new(2015,1,1)).to_i % individuals.length]
     individuals.shift(individuals.length / 3)
-    @person3 = individuals[(Date.today - Date.new(2015,1,1)).days% individuals.length]
+    @person3 = individuals[(Date.today - Date.new(2015,1,1)).to_i % individuals.length]
   end
 end
